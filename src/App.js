9c3ec25555f2e5d99210381e483ea282e710ad57
iImmersao <philippatchin@gmail.com>
2024-10-21 06:59:08 -0400

    Re-apply correction to initialisation of watched movies.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ export default function App() {
   //const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
-    return JSON.parse(storedValue);
+    if (storedValue) return JSON.parse(storedValue);
+    return [];
   });
 
   function handleSelectMovie(id) {
